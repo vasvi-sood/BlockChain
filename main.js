b13c8de7fe3fc0eb17427fa53efc466c524c97ba
Vasvi Sood <contactvasvisood@gmail.com>
2021-02-19 09:58:12 +0530

    changes

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
-let BlockChain =require ("./blockchain/blockchain");
-let hash = require("object-hash");
-let blockChain=new BlockChain();
+let database = require("./src/database");
+database.onConnect(() => {
 
-const PROOF = 1560; ///< Random Proof Number 
+    let BlockChain = require("./src/blockChain");
 
-//Check if the Proof is Valid
-let validProof = (proof) => {
-  let guessHash = hash(proof); ///< hash the proof
-  console.log("Hashing: ", guessHash);
-  return guessHash == hash(PROOF); ///< check against the real proof hash
-};
+    let blockChain = new BlockChain();
 
-let proofOfWork = () => {
-  let proof = 0;
-  //Loop till you get into the real proof
-  while(true) {
-    //check proof (Not valid, increment)
-    if(!validProof(proof)) {
-      proof++;
-    } else {
-      //We have got the proof, get out of the infinite loop;
-      break;
-    }
-  }
-  //Send Proof Back 
-  return proof;
-}
-if (proofOfWork() == PROOF) {
-    //if first block on chain (Previous hash is NULL)
-    let prevHash = blockChain.lastBock() ? blockChain.lastBock().hash : null;
-    //New Transactions 
-    blockChain.addNewTransaction("islem", "penywis", 560);
-    //Add Block
-    blockChain.addNewBlock(prevHash);
-}
+    let hash = require('object-hash');
 
-//Log Our Chain after block adding 
-console.log("Chain: ", blockChain.chain);
+    let PROOF = 1560;
+
+    /*if (proofOfWork() == PROOF) {
+              blockChain.addNewTransaction("islem", "alex", 200);
+              let prevHash = blockChain.lastBock() ?
+                  blockChain
+                  .lastBock()
+                  .hash :
+                  null;
+              blockChain.addNewBlock(prevHash);
+          }*/
+
+    blockChain.addNewTransaction("islem", "alex", 200);
+    blockChain.addNewBlock(null);
+
+    console.log("Chain : ", blockChain.chain);
+
+})
